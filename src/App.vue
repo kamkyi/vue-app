fed1791264115a5793ff63c6670e6b8007373be7
kamkyi <waihynhtun1994@gmail.com>
2020-11-09 21:06:56 +0630

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,33 @@
 <template>
-<b-container class="bv-example-row">
 <div>
-  <b-nav tabs>
-    <b-nav-item active>Active</b-nav-item>
-    <b-nav-item>Link</b-nav-item>
-    <b-nav-item>Another Link</b-nav-item>
-    <b-nav-item disabled>Disabled</b-nav-item>
-  </b-nav>
-</div>
+<b-container class="bv-example-row">
   <b-row>
-    <b-col>1 of 3</b-col>
-    <b-col>2 of 3</b-col>
-    <b-col>3 of 3</b-col>
+    <b-col>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. At odit eos tempore ad voluptate, inventore ea maiores nihil sequi hic nisi placeat, vitae dolores, harum numquam cumque distinctio eum. Corrupti?
+    </b-col>
+    <b-col>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum dolorum sequi totam. Exercitationem expedita porro ipsa ratione aspernatur, autem accusantium quas a officiis nulla rem voluptas, repellat minus cum tempora!
+    </b-col>
+    <b-col>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas mollitia quasi explicabo inventore necessitatibus minima? Officia porro doloremque ab molestias tempora recusandae reprehenderit labore, ut neque fugit quas, delectus soluta?
+    </b-col>
   </b-row>
 </b-container>
+
+<div class="container">
+   <div class="row">
+        <div class="col-md-4">
+           Lorem
+        </div>
+        <div class="col-md-4">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo mollitia dignissimos doloremque perspiciatis soluta voluptatum voluptates, magni accusamus facilis laborum velit animi quisquam quae magnam ad dolorum ratione non corrupti!
+        </div>
+        <div class="col-md-4">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores ipsam sequi quibusdam fugit iure libero optio totam modi fugiat iusto?
+        </div>
+    </div>
+</div>
+</div>
 </template>
 
 <script>
